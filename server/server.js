4aa6661f0d532ff93e0438fc5668204bebbab178
Nathandrake229 <39689729+Nathandrake229@users.noreply.github.com>
2019-06-28 16:40:06 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const path = require('path')
+const http = require('http')
 var pubpath = path.join(__dirname,'../public')
 const express = require('express')
-var port = process.env.PORT || 3000
+const socketio = require('socket.io')
+
+var port = process.env.port || 3000
 //console.log(pubpath)
 var app = express()
+var server = http.createServer(app)
+var io = socketio(server)
 app.use(express.static(pubpath))
-app.get('/', (req, res)=>{
-    res.send('it is up')
+io.on('connection', (socket)=>{
+    console.log('new user connected')
 })
-app.listen(port, ()=>{
+io.on('disconnect', (socket)=>{
+    console.log('disconnected')
+})
+server.listen(port, ()=>{
     console.log(`server is up ${port}`)
-})
+}) 
